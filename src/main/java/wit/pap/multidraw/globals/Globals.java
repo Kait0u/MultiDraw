db689ff419097c572754f3655f39c45deb20aeb9
Jakub Jaworski <jjaw.stuff@gmail.com>
2024-06-15 21:51:04 +0200

    New globals added.

--- src/main/java/wit/pap/multidraw/globals/Globals.java
+++ src/main/java/wit/pap/multidraw/globals/Globals.java
@@ -3,4 +3,9 @@ package wit.pap.multidraw.globals;
 public class Globals {
     public static final int IMAGE_WIDTH = 1920;
     public static final int IMAGE_HEIGHT = 1080;
+
+    public static final int WINDOW_INITIAL_WIDTH = 1270;
+    public static final int WINDOW_INITIAL_HEIGHT = 768;
+
+    public static final int WINDOW_MIN_DIM = 512;
 }
