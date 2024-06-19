d9e02a51cb2b4f06968d1ba8ddcd183945a10171
Jakub Jaworski <jjaw.stuff@gmail.com>
2024-06-19 02:54:25 +0200

    Got rid of old code.

--- .old.src/main/java/wit/pap/multidraw/server/Main.java
+++
@@ -1,8 +0,0 @@
-package wit.pap.multidraw.server;
-
-public class Main {
-    public static void main(String[] args) {
-        MultiDrawServer server = new MultiDrawServer(12345);
-        server.start();
-    }
-}
